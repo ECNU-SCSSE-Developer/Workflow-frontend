997983eb22e9e0cc484e19713db0c0a21b9088b0
Andrew <ybdong374@126.com>
2019-03-17 17:14:37 +0800

    some changes

--- workflow-miniprogram/miniprogram/pages/match/match.js
+++ workflow-miniprogram/miniprogram/pages/match/match.js
@@ -1,3 +1,4 @@
+var sliderWidth = 120; // 需要设置slider的宽度，用于计算中间位置
 Page({
 
   /**
@@ -9,7 +10,7 @@ Page({
       '/image/1.jpg',
       '/image/1.jpg'
     ],
-    tabs: ["近 期 比 赛", "已 截 止", "历 史 比 赛"],
+    tabs: ["近 期 比 赛", "已 截 止", "比 赛 年 鉴"],
     activeIndex: 0,
     sliderOffset: 0,
     sliderLeft: 0,
@@ -17,77 +18,93 @@ Page({
     matchs: [{
       id: 1,
       title: "比赛1",
+      level: "校级",
       time: "2019年2月28日",
     }, {
       id: 2,
       title: "比赛2",
+      level: "校级",
       time: "2019年2月28日",
     }, {
       id: 3,
       title: "比赛3",
+      level: "校级",
       time: "2019年2月28日",
     }, {
       id: 4,
       title: "比赛4",
+      level: "校级",
       time: "2019年2月28日",
     }, {
       id: 5,
       title: "比赛5",
+      level: "校级",
       time: "2019年2月28日",
     }, {
       id: 6,
       title: "比赛6",
+      level: "校级",
       time: "2019年2月28日",
     }, {
       id: 7,
       title: "比赛7",
+      level: "校级",
       time: "2019年2月28日",
     }, {
       id: 8,
       title: "比赛8",
+      level: "校级",
       time: "2019年2月28日",
     }, {
       id: 9,
       title: "比赛9",
+      level: "校级",
       time: "2019年2月28日",
     }, {
       id: 10,
       title: "比赛10",
+      level: "校级",
       time: "2019年2月28日",
     }],
-    historyMatchs: [{
-      id: 1,
-      title: "比赛1",
-      time: "2019年2月28日",
-    }, {
-      id: 2,
-      title: "比赛2",
-      time: "2019年2月28日",
-    }, {
-      id: 3,
-      title: "比赛3",
-      time: "2019年2月28日",
-    }],
+
     cutoffMatchs: [{
-      id: 1,
-      title: "比赛1",
-      time: "2019年2月28日",
-    }, {
-      id: 2,
-      title: "比赛2",
-      time: "2019年2月28日",
-    }, {
-      id: 3,
-      title: "比赛3",
-      time: "2019年2月28日",
-    }],
+        id: 1,
+        title: "比赛1",
+        level: "校级",
+        time: "2019年2月28日",
+      }, {
+        id: 2,
+        title: "比赛2",
+        level: "校级",
+        time: "2019年2月28日",
+      }, {
+        id: 3,
+        title: "比赛3",
+        level: "校级",
+        time: "2019年2月28日",
+      },
+      {
+        id: 4,
+        title: "比赛3",
+        level: "校级",
+        time: "2019年2月28日",
+      }
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
   },
 
   /**
--- workflow-miniprogram/miniprogram/pages/recruit/recruit.js
+++ workflow-miniprogram/miniprogram/pages/recruit/recruit.js
@@ -5,68 +5,116 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: [{
+      id: 1,
+      name: "xxx小队需要JAVA后端若干名",
+      match: "第六届创新创业大赛",
+      now: 5,
+      total: 10,
+      focus: 0,
+    }, {
+      id: 2,
+      name: "xxx小队需要JAVA后端若干名",
+      match: "第六届创新创业大赛",
+      now: 5,
+      total: 10,
+      focus: 1,
+    }, {
+      id: 3,
+      name: "xxx小队需要JAVA后端若干名",
+      match: "第六届创新创业大赛",
+      now: 5,
+      total: 10,
+      focus: 1,
+    }, {
+      id: 4,
+      name: "xxx小队需要JAVA后端若干名",
+      match: "第六届创新创业大赛",
+      now: 5,
+      total: 10,
+      focus: 0,
+    }, {
+      id: 5,
+      name: "xxx小队需要JAVA后端若干名",
+      match: "第六届创新创业大赛",
+      now: 5,
+      total: 10,
+      focus: 1,
+    }, {
+      id: 6,
+      name: "xxx小队需要JAVA后端若干名",
+      match: "第六届创新创业大赛",
+      now: 5,
+      total: 10,
+      focus: 0,
+    }, ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  
-  toDetail: function () {
-    setTimeout(()=>{wx.navigateTo({
-      url: '/pages/recruitDetail/recruitDetail',
-    })}, 500)
+
+  toDetail: function() {
+    setTimeout(() => {
+      wx.navigateTo({
+        url: '/pages/recruitDetail/recruitDetail',
+      })
+    }, 500)
+  },
+
+  changeFocus: function(e) {
+    console.info(e.currentTarget.dataset.id)
   },
 })
