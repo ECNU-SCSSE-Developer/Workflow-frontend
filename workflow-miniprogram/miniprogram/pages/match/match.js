ed3738955264b1387d236daf589a0148218d9a16
Andrew <ybdong374@126.com>
2019-03-20 19:34:57 +0800

    添加日历项

--- workflow-miniprogram/miniprogram/pages/match/match.js
+++ workflow-miniprogram/miniprogram/pages/match/match.js
@@ -1,4 +1,5 @@
 var sliderWidth = 120; // 需要设置slider的宽度，用于计算中间位置
+const MONTHS = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'June.', 'July.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
 Page({
 
   /**
@@ -15,77 +16,96 @@ Page({
     sliderOffset: 0,
     sliderLeft: 0,
     hasMatch: 1, //是否有近期比赛
+    cal_style: [],
+    year: new Date().getFullYear(), // 年份
+    month: new Date().getMonth() + 1, // 月份
+    day: new Date().getDate(),
+    str: MONTHS[new Date().getMonth()], // 月份字符串
     matchs: [{
       id: 1,
       img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m1.jpg",
       title: "第16届“大夏杯”大学生创业大赛",
       level: "校级",
-      time: "2019年2月28日",
+      time: "2019年4月28日",
     }, {
       id: 2,
       img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m2.jpg",
-        title: "R3全球大学生区块链商业计划竞赛",
+      title: "R3全球大学生区块链商业计划竞赛",
       level: "国家级",
-      time: "2019年2月28日",
+      time: "2019年4月28日",
     }, {
       id: 3,
       img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m3.jpg",
       title: "上海市大学生计算机应用能力大赛",
       level: "市级",
-      time: "2019年2月28日",
+      time: "2019年4月28日",
     }, {
       id: 4,
       img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m4.jpg",
       title: "(第十一届)华师大本科生计算机应用能力大赛",
       level: "校级",
-      time: "2019年2月28日",
+      time: "2019年4月28日",
     }, {
       id: 5,
       img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m6.jpg",
       title: "互联网+",
       level: "校级",
-      time: "2019年2月28日",
+      time: "2019年4月28日",
     }, {
       id: 6,
       img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m5.png",
       title: "创青春市赛",
       level: "市级",
-      time: "2019年2月28日",
+      time: "2019年4月28日",
     }],
 
     cutoffMatchs: [{
         id: 1,
-      img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m5.png",
-      title: "创青春国赛",
+        img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m5.png",
+        title: "创青春国赛",
         level: "国家级",
-        time: "2019年2月28日",
+        time: "2019年4月28日",
       }, {
         id: 2,
         img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m4.jpg",
         title: "互联网+",
         level: "市级",
-        time: "2019年2月28日",
+        time: "2019年4月28日",
       }, {
         id: 3,
         img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m3.jpg",
         title: "第2届计算机科学与软件工程学院双创比赛",
         level: "院级",
-        time: "2019年2月28日",
+        time: "2019年4月28日",
       },
       {
         id: 4,
         img: "https://workflow-1258575893.cos.ap-shanghai.myqcloud.com/m1.jpg",
         title: "第15届“大夏杯”大学生创业大赛",
         level: "校级",
-        time: "2019年2月28日",
+        time: "2019年4月28日",
       }
     ],
+    matchList: [{
+      id: 1,
+      name: "双创比赛",
+      time: "12月"
+    }, {
+      id: 2,
+      name: "大夏杯",
+      time: "5月2日"
+    }, {
+      id: 3,
+      name: "创青春",
+      time: "9月"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    const days_count = new Date(this.data.year, this.data.month, 0).getDate();
     var that = this;
     wx.getSystemInfo({
       success: function(res) {
@@ -95,6 +115,52 @@ Page({
         });
       }
     });
+    let cal_style = new Array;
+    for (let i = 1; i <= days_count; i++) {
+      const date = new Date(this.data.year, this.data.month - 1, i);
+      if (date.getDay() == 0) {
+        cal_style.push({
+          month: 'current',
+          day: i,
+          color: '#f488cd'
+        });
+      } else {
+        cal_style.push({
+          month: 'current',
+          day: i,
+          color: '#a18ada'
+        });
+      }
+    }
+    cal_style.push({
+      month: 'current',
+      day: 12,
+      color: 'white',
+      background: '#b49eeb'
+    });
+    cal_style.push({
+      month: 'current',
+      day: 17,
+      color: 'white',
+      background: '#f5a8f0'
+    });
+    cal_style.push({
+      month: 'current',
+      day: 20,
+      color: 'white',
+      background: '#aad4f5'
+    });
+    cal_style.push({
+      month: 'current',
+      day: 25,
+      color: 'white',
+      background: '#84e7d0'
+    });
+
+    this.setData({
+      cal_style: cal_style
+    });
+
   },
 
   /**
@@ -108,7 +174,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    this.setData({
+      activeIndex: 0,
+      sliderOffset: 0
+    })
   },
 
   /**
